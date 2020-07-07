2bf085fb8a7dafd7745290e56714d34884419b38
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-07 20:07:05 +0800

    database created

---
+++ sql/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE imgupload
+
+CREATE TABLE users(
+    id int(11) not NULL PRIMARY KEY AUTO_INCREMENT,
+    first varchar(256) not NULL,
+    last varchar(256) not NULL,
+    username varchar(256) not NULL,
+    password varchar(256) not NULL,
+);
+
+CREATE TABLE profileimg(
+    id int(11) not NULL PRIMARY KEY AUTO_INCREMENT,
+    userid int(11) not NULL,
+    status int(11) not NULL
+);
