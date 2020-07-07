c714e17a7813f9db8a204895187dae0fd36ef55a
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-07 21:21:44 +0800

    login / logout file

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include_once 'dbh.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,9 +9,31 @@
         <title>PHP Training</title>
     </head>
     <body>
-        <form action="upload.php" method="POST" enctype="multipart/form-data">
-            <input type="file" name="file">
-            <button type="submit" name="submit">UPLOAD</button>
+
+        <?php
+            if(isset($_SESSION['id'])){
+                if($_SESSION['id'] == 1){
+                    echo "you are logged in as user #1";
+                }
+
+                echo "<form action='upload.php' method='POST' enctype='multipart/form-data'>
+                        <input type='file' name='file'>
+                        <button type='submit' name='submit'>UPLOAD</button>
+                    </form>";
+            }
+            else{
+                echo "You are not logged in";
+            }
+        ?>
+
+        <p>Login as user!</p>
+        <form action="login.php" method="post">
+            <button type="submit" name="submitLogin">Login</button>
+        </form>
+
+        <p>Logout as user!</p>
+        <form action="logout.php" method="post">
+            <button type="submit" name="submitLogout">Logout</button>
         </form>
     </body>
 </html>
