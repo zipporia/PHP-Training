0bb69011afccd480e07af1d96fe4ac992a11db8b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-07-30 22:30:24 +0800

    Inheritance
    PHP OOP

---
+++ OOP/inheritance.php
@@ -0,0 +1,30 @@
+<?php
+
+Class Fruit{
+    protected $name;
+    public $color;
+    private $size;
+
+    public function __construct($name, $color, $size){
+        $this->name = $name;
+        $this->color = $color;
+        $this->size = $size;
+    }
+
+    public function intro(){
+        echo "The fruit is {$this->name} and the color is {$this->color} and the size is {$this->size}.";
+    }
+}
+
+Class Strawberry extends Fruit{
+    public function message(){
+        echo "this is {$this->name} class";
+        echo "<br>";
+        echo "the color is {$this->color}";
+        echo "<br>";
+    }
+}
+
+$strawberry = new Strawberry("Strawberry", "red", 10);
+$strawberry->message();
+$strawberry->intro();
