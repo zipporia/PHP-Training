9e7998d581d84645be4fd41f04dbe261014efed0
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-03 23:20:31 +0800

    css added

--- crud/index.php
+++ crud/index.php
@@ -7,8 +7,22 @@
 <head>
     <meta charset="utf-8" />
     <title>CRUD</title>
+    <link rel="stylesheet" href="php_action/style.css">
+
 </head>
 <body>
-    
+
+    <div class="manageMember">
+        <a href="create.php"><button type="button">Add Member</button></a>
+        <table cellspacing="0" cellpadding="0" border="1">
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Contact</th>
+                <th>Option</th>
+            </tr>
+        </table>
+    </div>
+
 </body>
 </html>
--- crud/php_action/db_connect.php
+++ crud/php_action/db_connect.php
@@ -9,6 +9,4 @@ $conn = new mysqli($dblocalhost, $dbuser, $dbpassowrd, $dbname);
 
 if($conn->connect_error){
     die("Connection Failed: " .$conn->connect_error);
-}else{
-    echo "Successfully Connected";
 }
