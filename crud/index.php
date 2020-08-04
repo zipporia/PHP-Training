e36a7cf142dc1746f5066376516a98c7ac4c6d72
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-08-04 14:04:11 +0800

    fetch data from database

--- crud/index.php
+++ crud/index.php
@@ -8,19 +8,40 @@
     <meta charset="utf-8" />
     <title>CRUD</title>
     <link rel="stylesheet" href="php_action/style.css">
-
 </head>
 <body>
 
     <div class="manageMember">
         <a href="create.php"><button type="button">Add Member</button></a>
         <table cellspacing="0" cellpadding="0" border="1">
-            <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Contact</th>
-                <th>Option</th>
-            </tr>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Contact</th>
+                    <th>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = "SELECT * FROM members WHERE active = 1";
+                    $result = $conn->query($sql);
+
+                    if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){
+                            echo "<tr>
+                                <td>" . $row['fname'] . " " . $row['lname'] . "</td>
+                                <td>" . $row['contact'] . "</td>
+                                <td>" . $row['age'] . "</td>
+                                <td>
+                                    <a href='edit.php?id=".$row['id']."'> <button type='button'> Edit </button></a>
+                                    <a href='remove.php?id=".$row['id']."'> <button type='button'> Remove </button></a>
+                                </td>
+                            </tr>";
+                        }
+                    }
+                ?>
+            </tbody>
         </table>
     </div>
 
